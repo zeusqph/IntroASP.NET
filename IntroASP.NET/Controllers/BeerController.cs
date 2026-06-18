d425e8ff46bbdc4706bba24041223cec1283a442
Zeus Hermes <zeus.hermes@email.com>
2026-06-17 20:01:19 -0500

    Se agrego un controlador y su vista

--- IntroASP.NET/Controllers/BeerController.cs
+++ IntroASP.NET/Controllers/BeerController.cs
@@ -4,23 +4,27 @@ using Microsoft.EntityFrameworkCore;
 
 namespace IntroASP.NET.Controllers
 {
-    // Controlador encargado de mostrar las marcas.
-    public class BrandController : Controller
+    // Controlador encargado de mostrar las cervezas.
+    public class BeerController : Controller
     {
 
         // Contexto de la base de datos.
         private readonly PubContext _context;
 
         // Constructor que recibe el contexto mediante inyección de dependencias.
-        public BrandController(PubContext context)
+        public BeerController(PubContext context)
         {
             _context = context;
         }
 
-        // Obtiene y muestra la lista de marcas registradas.
+        // Muestra la lista de cervezas junto con su marca.
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Brands.ToListAsync());
+            // Obtiene las cervezas e incluye la información de la marca.
+            var beers = _context.Beers.Include(b => b.Brand);
+            // Envía la lista a la vista.
+            return View(await beers.ToListAsync());
         }
+
     }
 }
